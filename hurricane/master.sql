ed1a92b072765000d6a5770199533fb5706bb9cc
William Clark <wfclark2@gmail.com>
2016-05-19 18:25:40 -0400

    Create master.sql

---
+++ hurricane/master.sql
@@ -0,0 +1,37 @@
+drop table if exists hurricane_irene
+
+create table hurricane_irene as
+select gid, serial_num, season, basin, sub_basin, name, iso_time, wmo_wind, wmo_pres, 'wmo_wind_%' as wmo_wind_radii , 
+atc_rmw , atc_poci , atc_roci , atc_eye , atc_w34_r1 , atc_w34_r2 , atc_w34_r3 , atc_w34_r4 ,atc_w50_r1 ,
+atc_w50_r2 , atc_w50_r3 ,atc_w50_r4 ,atc_w64_r1 ,atc_w64_r2, atc_w64_r3 , atc_w64_r4, geom from allstormspts_4326 where name = 'IRENE'
+
+drop view if exists vw_quadrent_1;
+
+drop view if exists vw_quadrent_2;
+
+drop view if exists vw_quadrent_3;
+
+drop view if exists vw_quadrent_4;
+
+create or replace view vw_quadrent_1 as 
+select gid, st_buffer(geom, (select distinct atc_w34_r1 from hurricane_irene limit 1))::geometry(polygon,4326) as geom from hurricane_irene limit 1;
+
+create or replace view vw_quadrent_2 as
+select gid, st_buffer(geom, (select distinct atc_w34_r2 from hurricane_irene limit 1))::geometry(polygon,4326) as geom from hurricane_irene limit 1;
+
+create or replace view vw_quadrent_3 as
+select gid, st_buffer(geom, (select distinct atc_w34_r3 from hurricane_irene limit 1))::geometry(polygon,4326) as geom from hurricane_irene limit 1;
+
+create or replace view vw_quadrent_4 as
+select gid, st_buffer(geom, (select distinct atc_w34_r4 from hurricane_irene limit 1))::geometry(polygon,4326) as geom from hurricane_irene limit 1;
+
+create or replace view vw_quadrent_12 as
+select st_union(a.geom,b.geom) ::geometry(polygon,4326) as geom from vw_quadrent_1 as a, vw_quadrent_2 as b;
+
+create or replace view vw_quadrent_34 as
+select st_union(a.geom,b.geom) ::geometry(polygon,4326) as geom from vw_quadrent_3 as a, vw_quadrent_4 as b
+
+
+create or replace view vw_quadrent_1234 as
+select st_union(a.geom,b.geom) ::geometry(polygon,4326) as geom from vw_quadrent_12 as a, vw_quadrent_34 as b
+
