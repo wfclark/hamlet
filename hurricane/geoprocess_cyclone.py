c4682b4378608f7e71b9bcf2510ad1cc6042424f
William Clark <wfclark2@gmail.com>
2016-05-21 17:45:57 -0400

    Update geoprocess_cyclone.py

--- hurricane/geoprocess_cyclone.py
+++ hurricane/geoprocess_cyclone.py
@@ -1,8 +1,6 @@
 import sys
 import os
-import urllib2
 import datetime
-import time
 import psycopg2
 import pandas
 from subprocess import call, Popen
@@ -19,9 +17,13 @@ except Exception as e:
 
 print "Connected!\n"
 
+hurricane_name = 'SANDY'
+
 dataframe_cur = conn.cursor()
 
-dataframe_cur.execute("""Select * from hurricane_katrina""")
+dataframe_sql = """Select * from hurricane_{}""".format(hurricane_name)
+
+dataframe_cur.execute(dataframe_sql)
 
 data = dataframe_cur.fetchall()
 
@@ -33,12 +35,23 @@ dataframe.columns = colnames
 
 conn.commit()
 
-buffer_cur = conn.cursor()
+range_feat =  range(len(dataframe))
 
-for key in range(1,len(dataframe) + 1):
+range_feat_strp = str(range_feat).strip('[]')
+
+range_feat_strp_v2 = range_feat_strp.split(',')
+
+print range_feat_strp_v2
+
+bash_syntax = ' ' 
+
+for data in range_feat:
+	bash_syntax += ' ' + str(data)
+
+for key in range(1,len(dataframe)-1):
 	
 	sql = """create or replace view vw_rmw_{} as
-	select iso_time, ogc_fid, st_transform(st_buffer(st_transform(wkb_geometry,32612), (select distinct atc_roci from katrina_{})*1069),4326)::geometry(polygon, 4326) as geom from katrina_{} limit 1;""".format(key, key, key)
+	select iso_time, ogc_fid, st_transform(st_buffer(st_transform(wkb_geometry,32612), (select distinct atc_roci from {}_{})*1069),4326)::geometry(polygon, 4326) as geom from {}_{} limit 1;""".format(key, hurricane_name, key, hurricane_name, key)
 
 	print sql 
 
@@ -47,12 +60,12 @@ for key in range(1,len(dataframe) + 1):
 
 update_cur = conn.cursor() 
 
-for key in range(1, len(dataframe) + 1):
+for key in range(1, len(dataframe)-1):
 	
- 	sql = """update hurricane_katrina_geo as a
+ 	sql = """update hurricane_{}_geo as a
  	set geom = b.geom 
  	from vw_rmw_{} as b
- 	where a.iso_time = b.iso_time""".format(key) 
+ 	where a.iso_time = b.iso_time""".format(hurricane_name, key) 
 
  	print sql 
 
