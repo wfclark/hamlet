1a921f5adf8dac59a8d1eec9cc4d16ffb1bd5998
William Clark <wfclark2@gmail.com>
2016-05-22 23:26:19 -0400

    Update geoprocess_members.py

--- hurricane/geoprocess_members.py
+++ hurricane/geoprocess_members.py
@@ -7,6 +7,9 @@ from subprocess import call, Popen
 
 conn_string = "dbname='hamlethurricane' user=postgres port='5432' host='127.0.0.1' password='password'"
 
+os.system("exit")
+os.system("exit")
+
 print "Connecting to database..."
 
 try:
@@ -35,13 +38,12 @@ dataframe.columns = colnames
 
 conn.commit()
 
-range_feat =  range(len(dataframe))
+range_feat =  range(len(dataframe)-1)
 
 range_feat_strp = str(range_feat).strip('[]')
 
 range_feat_strp_v2 = range_feat_strp.split(',')
-print
- range_feat_strp_v2
+print range_feat_strp_v2
 
 drop_if_geo_sql = """drop table if exists hurricane_{}_geo""".format(hurricane_name)
 
@@ -99,7 +101,7 @@ intersect_cur = conn.cursor()
 for key in range(1,len(dataframe)-1):
 	
 	sql = """create or replace view vw_impact_{} as
-	select b.iso_time, b.ogc_fid, sum(parval) as impact, a.geom::geometry(polygon, 4326) as geom 
+	select b.iso_time, b.ogc_fid, sum(parval) as impact, a.geom::geometry(multipolygon, 4326) as geom 
 	from dare_4326 as a join vw_rmw_{} as b on st_intersects(a.geom,b.geom)
 	group by b.iso_time, b.ogc_fid, a.geom;""".format(key, key)
 
@@ -114,7 +116,7 @@ update_cur = conn.cursor()
 for key in range(1, len(dataframe)-1):
 	
  	sql = """update hurricane_{}_geo as a
- 	set geom = b.geom 
+ 	set geom = b.geom
  	from vw_rmw_{} as b
  	where a.iso_time = b.iso_time""".format(hurricane_name, key) 
 
@@ -122,6 +124,8 @@ for key in range(1, len(dataframe)-1):
 
  	update_cur.execute(sql)
  	conn.commit()
+
+impact_cur = conn.cursor()
  
 for key in range(1, len(dataframe)-1):
 	
@@ -132,5 +136,5 @@ for key in range(1, len(dataframe)-1):
 
  	print sql 
 
- 	update_cur.execute(sql)
+ 	impact_cur.execute(sql)
  	conn.commit()
