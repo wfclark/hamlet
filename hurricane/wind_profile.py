bc4a030c0cb4455aa5377c7d6d3bc59884aa9363
William Clark <wfclark2@gmail.com>
2016-06-20 11:58:15 -0400

    Update wind_profile.py

--- hurricane/wind_profile.py
+++ hurricane/wind_profile.py
@@ -19,24 +19,33 @@ import matplotlib
 
 df = pd.read_csv('hurricane_test_v2.dat.gz', compression = 'gzip', sep=',' , error_bad_lines=False)
 
+df
+
 print df
 
 datetime = df.iloc[:,5:6]
+print datetime
 forecast_time = df.iloc[:,2:3]
+
+print forecast_time
 pn = 101000
-pc = df.iloc[:,9:10]
-Vmax = df.iloc[:,8:9]
-Rmax = df.iloc[:,19:20]
-roci = df.iloc[:,18:19]
-Vtrans = df.iloc[:,23:24]
-lat = df.iloc[:,6:7].replace(to_replace='N', value='', regex=True).replace(to_replace='S', value='', regex=True)
+pc = df.fillna(0).iloc[:,9:10]
+print pc
+Vmax = df.fillna(0).iloc[:,8:9]
+print Vmax
+
+Rmax = df.fillna(0).iloc[:,19:20]
+print Rmax
+roci = df.fillna(0).iloc[:,18:19] 
+Vtrans = df.fillna(0).iloc[:,23:24]
+lat = df.fillna(0).iloc[:,6:7].replace(to_replace='N', value='', regex=True).replace(to_replace='S', value='', regex=True)
+print lat
 
 pc_float = (np.float64(pc.values[5]).item()) * 100 
-print pc_float 
-Vmax_float = (np.float64(Vmax.values[5]).item())
-Rmax_float = (np.float64(Rmax.values[5]).item())
-roci_float = (np.float64(roci.values[5]).item())
-Vtrans_float = (np.float64(Vtrans.values[5]).item())
+Vmax_float = (np.float64(Vmax.values[5]).item()) / .514
+Rmax_float = (np.float64(Rmax.values[5]).item()) / .539
+roci_float = (np.float64(roci.values[5]).item()) / .514
+Vtrans_float = (np.float64(Vtrans.values[5]).item()) / .514
 theta_float = (np.float64(lat.values[5]).item())
 
 
@@ -61,8 +70,8 @@ radius_roci = []
 print range(1,int(roci_float))
 
 
-for i in range(1,int(roci_float)):
-	Vg = (mp.sqrt(((A*B*(pn - int(pc_float)))*mp.exp(int(-A)/int(i)**int(B))/(((int(rho)*int(i)**int(B) + int(i)**2*int(f)**2/4))))) - (int(i)* int(f)/2))
+for i in range(1,int(roci_float),1):
+	Vg = (mp.sqrt((((A*B*(pn - int(pc_float)))*mp.exp(int(-A)/(int(i)**int(B))))/(((int(rho)*int(i)**int(B) + ((int(i)**2*int(f)**2)/4))))))) - (((int(i)* int(f))/2))
 	wind_speed.append(float(Vg))
 	radius_roci.append(i)
 	print Vg
@@ -71,18 +80,20 @@ for i in range(1,int(roci_float)):
 df_windspeed = pd.DataFrame(wind_speed)
 df_radius = pd.DataFrame(radius_roci)
 
-plot_dataframe = pd.DataFrame({'Radius' : radius_roci, 'Windspeed' : wind_speed})
+plot_dataframe = pd.DataFrame({'Radius (km)' : radius_roci, 'Windspeed' : wind_speed})
 
-print plot_dataframe
-print datetime
-print forecast_time
+#print plot_dataframe
+#print datetime
+#print forecast_time
 
 plt.figure()
 
-wind_speed_plot = plot_dataframe.plot(x ='Radius', y='Windspeed', label = 'Computer Wind Speed (m/s)'); plt.legend(loc ='best')
+wind_speed_plot = plot_dataframe.plot(x ='Radius (km)', y='Windspeed', label = 'Computed Wind Speed (m/s)'); plt.legend(loc ='best')
 
 wind_speed_plot.set_ylabel('Wind Speed (m/s)')
+wind_speed_plot.set_xlabel('Radius (km)')
 
 plt.show()
+
 
 import matplotlib.pyplot as plt
